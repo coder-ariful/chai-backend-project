0fdc76df6c95747e32dca359c9f4635784ad050f
sohan-me-01 <sohanshak850@gmail.com>
2025-07-13 00:20:19 +0600

    add user and video models and installs some new dev

---
+++ src/models/user.model.js
@@ -0,0 +1,95 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"; // for encode-text convert to text
+import bcrypt from "bcrypt" // for text convert to encode-text 
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true, //  auto matic gap or space removed it.
+        index: true  // for searching topic .
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true,
+        trim: true, //  auto matic gap or space removed it.
+    },
+    fullName: {
+        type: String,
+        require: true,
+        trim: true, //  auto matic gap or space removed it.
+        index: true  // for searching topic .
+    },
+    avatar: {
+        type: String, // cloudinary url here only .
+        require: true
+    },
+    coverImage: {
+        type: String, // cloudinary url
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String,
+        require: [true, 'Password is required'],
+    },
+    refreshToken: {
+        type: String
+    }
+}, { timestamps: true });
+
+// userSchema.pre("save",()=> {})  // this function is wrong because "we need to use 'this' word!!!!"
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+// creating a method for checking password is matched or not
+userSchema.methods.isPasswordCorrect = async function name(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+// creating jwt token with accessToken and refreshToken.
+userSchema.methods.generateAccessToken = function () {
+    //sign method create the main token.
+    // ======================== if you need await or async then we can add it ===========================
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    //sign method create the main token.
+    // ======================== if you need await or async then we can add it ===========================
+    return jwt.sign(
+        {
+            _id: this._id,
+            // email: this.email,
+            // username: this.username,
+            // fullname: this.fullname
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User', userSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, // cloudinary url
+        require : true
+    },
+    thumbnail: {
+        type : String, // cloudinary url
+        require : true
+    },
+    title : {
+        type: String,
+        require : true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    duration:{
+        type: Number, // we get duration in 'cloudinary'
+        require: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    videoOwner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+},{timestamps : true});
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model('Video',videoSchema)
